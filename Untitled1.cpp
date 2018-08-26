e10e97bf33d1e704ea9207e207e82b0e73d71a02
OluwadaraDaily <42414929+OluwadaraDaily@users.noreply.github.com>
2018-08-26 19:03:49 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  int i;
+  char buffer [33];
+  printf ("Enter a number: ");
+  scanf ("%d",&i);
+  itoa (i,buffer,2);
+  printf ("binary: %s\n",buffer);
+  return 0;
+}
